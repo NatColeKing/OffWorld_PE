e8efc9849d3cd2bea2f3b0319fff53b99d14b9b6
Nat <natalie1vm@hotmail.com>
2019-02-04 17:23:43 -0800

    Skyboxes added

---
+++ Assets/Scripts/geyser.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class geyser : MonoBehaviour {
+
+	public ParticleSystem Bubbles;
+	public ParticleSystem MainColumn;
+
+	public float minTimeActive = 1f;
+	public float maxTimeActive = 5f;
+	public float minTimeInactive = 5f;
+	public float maxTimeInactive = 10f;
+	public float bubbleLingerDuration = 1f;
+
+	// Use this for initialization
+	void Start ()
+	{
+		//MainColumn = GetComponentInChildren<geyser_ColumnController>().GetComponent<ParticleSystem>();
+		//Bubbles = transform.Find("Bubbles").GetComponent<ParticleSystem>();
+		Bubbles.Stop();
+		DisableColumn();
+	}
+
+	void DisableColumn()
+	{
+			// Stop Column animation
+			MainColumn.Stop();
+			Invoke("DisableBubbles", bubbleLingerDuration);
+	}
+
+	void DisableBubbles()
+	{
+			// Stop bubble animation
+			Bubbles.Stop();
+			float delay = Random.Range(minTimeInactive, maxTimeInactive);
+			Invoke("EnableBubbles", delay);
+	}
+
+	void EnableBubbles()
+	{
+			//Start bubble animation
+			Bubbles.Play();
+			Invoke("EnableColumn", bubbleLingerDuration);
+	}
+
+	void EnableColumn()
+	{
+			//Start geyser animation
+			MainColumn.Play();
+			float delay = Random.Range(minTimeActive, maxTimeActive);
+			Invoke("DisableColumn", delay);
+	}
+}
---
+++ Assets/Scripts/geyser_ColumnController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class geyser_ColumnController : MonoBehaviour {
+
+    private const float COLUMN_FORCE_MULTIPLIER = 10f;
+    public float columnStrength;
+    public float columnHeight;
+
+    private void Start()
+    {
+        transform.localScale = new Vector3(transform.localScale.x, columnHeight, transform.localScale.z);
+        transform.localPosition = new Vector3(transform.localPosition.x, columnHeight / 2 + .5f, transform.localPosition.z); // adding .5 to account for height of base
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (CheckIsShip(other))
+            ApplyForce(other);
+    }
+
+    private bool CheckIsShip(Collider other)
+    {
+        return (other.tag == "Player" || other.tag == "Ship");
+    }
+
+    private void ApplyForce(Collider other)
+    {
+        Rigidbody shipRigidbody = other.GetComponent<Rigidbody>();
+        float distance = DistanceFromBase(other);
+        float forceMultiplier = COLUMN_FORCE_MULTIPLIER * columnStrength / Mathf.Pow(distance, 2f);
+        shipRigidbody.AddForce(forceMultiplier * transform.up);
+    }
+
+    private float DistanceFromBase(Collider other)
+    {
+        Vector3 shipPosition = other.gameObject.transform.position;
+        Vector3 basePosition = BasePosition();
+        float distance = Vector3.Distance(shipPosition, basePosition);
+        distance = Mathf.Abs(distance);
+        return distance;
+    }
+
+    private Vector3 BasePosition()
+    {
+        Vector3 basePosition = new Vector3(transform.position.x, (transform.position.y - (columnHeight / 2)), transform.position.z);
+        return basePosition;
+    }
+}
